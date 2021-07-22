051c8c83ea9750dcb7f00990c0e54cb9f442d08d
iqaenganer <daw.camalian@yandex.ru>
2021-07-22 09:50:45 +0300

    com8

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
     public int calculate(int price) {
         int bonus = 20;
-        int bonusMiles = 2;
+        int bonusMiles = 1;
         int miles = price * bonusMiles / bonus ;
         return miles;
     }
